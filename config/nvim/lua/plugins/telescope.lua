afdd4e650c14056514243c9856abf44e2abbbb5b
ryoh827 <ryoh827.dev@gmail.com>
2025-02-24 01:35:10 +0900

    feat: add keymap

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,16 @@
 return {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('telescope').setup({
+        pickers = {
+          find_files = {
+            find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+          }
+        }
+      })
+    end,
   },
   {
     "smartpde/telescope-recent-files"
