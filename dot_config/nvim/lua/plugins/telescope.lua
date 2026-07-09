09a9333d8410abcbaa1f2b9e3cf7795861bf82c3
ryoh827 <15053874+ryoh827@users.noreply.github.com>
2026-07-09 22:30:13 +0900

    feat: nvim周りの快適性を改善
    設定構成の整理(package.pathハック除去・mason重複/死にコード削除)に加え、
    プラグイン追加(snacks有効化/treesitter-textobjects/context/fzf-native/trouble/
    grug-far/persistence)、which-keyグループ登録、autocmd(ヤンクHL・カーソル復元・
    autoread)、gitsigns hunkキーマップ、format-on-save等をまとめて導入。

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,20 @@
 return {
   {
     'nvim-telescope/telescope.nvim', branch = 'master',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-ui-select.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope-ui-select.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
     config = function()
       require('telescope').setup({
+        defaults = {
+          vimgrep_arguments = {
+            "rg", "--color=never", "--no-heading", "--with-filename",
+            "--line-number", "--column", "--smart-case",
+            "--hidden", "--glob", "!**/.git/*",
+          },
+        },
         pickers = {
           find_files = {
             find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
@@ -16,6 +27,7 @@ return {
         },
       })
       require('telescope').load_extension('ui-select')
+      require('telescope').load_extension('fzf')
     end,
   },
   {
