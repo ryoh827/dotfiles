df2767e128b21a1dffabce4c48249a42b85f356b
ryoh827 <15053874+ryoh827@users.noreply.github.com>
2026-07-16 11:55:57 +0900

    feat: nvim補完の確定キーをsuper-tabプリセットに変更

--- dot_config/nvim/lua/plugins/completion.lua
+++ dot_config/nvim/lua/plugins/completion.lua
@@ -4,7 +4,7 @@ return {
     version = "1.*",
     event = "InsertEnter",
     opts = {
-      keymap = { preset = "default" },
+      keymap = { preset = "super-tab" },
       appearance = { nerd_font_variant = "mono" },
       completion = {
         documentation = { auto_show = true, auto_show_delay_ms = 200 },
