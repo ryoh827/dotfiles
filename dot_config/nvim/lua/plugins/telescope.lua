db9d8b65c98da58b0b8e5fa488843a313461c287
ryoh827 <15053874+ryoh827@users.noreply.github.com>
2026-07-24 13:54:30 +0900

    feat: use telescope-live-grep-args for dynamic exclude globs
    <leader>fg now uses telescope-live-grep-args.nvim so rg flags typed
    in the prompt (e.g. `-g !spec/**`) actually filter results; vanilla
    live_grep passed the whole prompt as a literal search pattern.

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
       'nvim-lua/plenary.nvim',
       'nvim-telescope/telescope-ui-select.nvim',
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      { 'nvim-telescope/telescope-live-grep-args.nvim', version = '^1.0.0' },
     },
     config = function()
       require('telescope').setup({
@@ -28,6 +29,7 @@ return {
       })
       require('telescope').load_extension('ui-select')
       require('telescope').load_extension('fzf')
+      require('telescope').load_extension('live_grep_args')
     end,
   },
   {
